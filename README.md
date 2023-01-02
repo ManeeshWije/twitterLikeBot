f93a6320609004a8f9b730926012584abb9f6f4d
ManeeshWije <m.mwije1@gmail.com>
2023-01-01 19:09:51 -0500

    re-visit this

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # twitterLikeBot
 - Python script which opens browser, logs into users twitter, searches up keyword, and likes all corresponding tweets
-    
-NOTE:
-
 - You may have to download geckodriver and add it to your path or if on linux/unix, copy it to /usr/local/bin
 - When using script, make sure to fill in username and password as well as keyword portion of the code!
+
+NOTE:
+- A better use of your time would probably to use tweepy instead of automation through Selenium as Twitter has updated their classnames to be dynamic. This was just a small project I made back in 2020 which still needs to be updated
--- bot.py
+++ bot.py
@@ -3,49 +3,55 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
+
 class TwitterBot:
-    def __init__(self,username,password):
+    def __init__(self, username, password):
         self.username = username
         self.password = password
-        self.bot = webdriver.Firefox()
+        self.bot = webdriver.Chrome()
 
     def login(self):
         bot = self.bot
-        bot.get('https://twitter.com/i/flow/login')
-        time.sleep(3)
-        email=bot.find_element_by_name('username')
+        bot.get("https://twitter.com/i/flow/login")
+        time.sleep(10)
+        email = bot.find_element(By.TAG_NAME, "input")
         email.clear()
         email.send_keys(self.username)
         email.send_keys(Keys.RETURN)
-        time.sleep(3)
-        password=bot.find_element_by_name('password')
+        time.sleep(5)
+        password = bot.find_element(
+            By.XPATH,
+            '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input',
+        )
         password.clear()
         password.send_keys(self.password)
         password.send_keys(Keys.RETURN)
-        time.sleep(3)
+        time.sleep(5)
 
     def like_tweet(self, hashtag):
         bot = self.bot
-        bot.get('https://twitter.com/search?q='+hashtag+'&src=typd')
-        time.sleep(3)
+        bot.get("https://twitter.com/search?q=" + hashtag + "&src=typd")
+        time.sleep(5)
         for i in range(1, 3):
-            bot.execute_script('window.scrollTo(0,document.body.scrollHeight)')
-            time.sleep(2)
-            tweetLinks = [i.get_attribute('href')
-                          for i in bot.find_elements_by_xpath("//a[@dir='auto']")]
-            filteredLinks = list(filter(lambda x: 'status' in x, tweetLinks))
+            bot.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+            time.sleep(5)
+            tweetLinks = [
+                i.get_attribute("href")
+                for i in bot.find_elements(By.XPATH, "//a[@dir='auto']")
+            ]
+            filteredLinks = list(filter(lambda x: "status" in x, tweetLinks))
             print(filteredLinks)
 
             for link in filteredLinks:
                 bot.get(link)
                 time.sleep(5)
                 try:
-                    bot.find_element_by_xpath("//div[@data-testid='like']").click()
+                    bot.find_element(By.XPATH, "//div[@data-testid='like']").click()
                     time.sleep(10)
                 except Exception as ex:
                     time.sleep(10)
 
 
-maneesh = TwitterBot("USERNAME", "PASSWORD")
-maneesh.login()
-maneesh.like_tweet('KEYWORD')
+user = TwitterBot("USERNAME", "PASSWORD")
+user.login()
+user.like_tweet("programming")
